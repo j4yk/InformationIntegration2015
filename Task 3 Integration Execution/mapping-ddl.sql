ca86a4c36b477d11bd4380a65d0df73476f26515
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2015-12-04 13:47:30 +0100

    added gnd mapping; updated mapping ddl

--- Task 3 Integration Execution/mapping-ddl.sql
+++ Task 3 Integration Execution/mapping-ddl.sql
@@ -61,6 +61,54 @@ CREATE TABLE aw_person_id (
 ALTER TABLE aw_person_id OWNER TO postgres;
 
 --
+-- Name: gnd_occupation_id; Type: TABLE; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE gnd_occupation_id (
+    name character varying NOT NULL,
+    id integer
+);
+
+
+ALTER TABLE gnd_occupation_id OWNER TO postgres;
+
+--
+-- Name: gnd_person_id; Type: TABLE; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE gnd_person_id (
+    gnd_id character varying NOT NULL,
+    id integer
+);
+
+
+ALTER TABLE gnd_person_id OWNER TO postgres;
+
+--
+-- Name: gnd_place_id; Type: TABLE; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE gnd_place_id (
+    gnd_id character varying NOT NULL,
+    id integer
+);
+
+
+ALTER TABLE gnd_place_id OWNER TO postgres;
+
+--
+-- Name: gnd_work_id; Type: TABLE; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE gnd_work_id (
+    gnd_id character varying NOT NULL,
+    id integer
+);
+
+
+ALTER TABLE gnd_work_id OWNER TO postgres;
+
+--
 -- Name: aw_occupation_id_pkey; Type: CONSTRAINT; Schema: mapping; Owner: postgres; Tablespace: 
 --
 
@@ -82,6 +130,80 @@ ALTER TABLE ONLY aw_party_id
 
 ALTER TABLE ONLY aw_person_id
     ADD CONSTRAINT aw_person_id_pkey PRIMARY KEY (aw_uuid);
+
+
+--
+-- Name: gnd_occupation_id_pkey; Type: CONSTRAINT; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY gnd_occupation_id
+    ADD CONSTRAINT gnd_occupation_id_pkey PRIMARY KEY (name);
+
+
+--
+-- Name: gnd_person_id_pkey; Type: CONSTRAINT; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY gnd_person_id
+    ADD CONSTRAINT gnd_person_id_pkey PRIMARY KEY (gnd_id);
+
+
+--
+-- Name: gnd_place_id_pkey; Type: CONSTRAINT; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY gnd_place_id
+    ADD CONSTRAINT gnd_place_id_pkey PRIMARY KEY (gnd_id);
+
+
+--
+-- Name: gnd_work_id_pkey; Type: CONSTRAINT; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY gnd_work_id
+    ADD CONSTRAINT gnd_work_id_pkey PRIMARY KEY (gnd_id);
+
+
+--
+-- Name: fki_aw_occupation_id_fkey; Type: INDEX; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE INDEX fki_aw_occupation_id_fkey ON aw_occupation_id USING btree (id);
+
+
+--
+-- Name: fki_aw_party_id_fkey; Type: INDEX; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE INDEX fki_aw_party_id_fkey ON aw_party_id USING btree (id);
+
+
+--
+-- Name: fki_gnd_occupation_id_fkey; Type: INDEX; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE INDEX fki_gnd_occupation_id_fkey ON gnd_occupation_id USING btree (id);
+
+
+--
+-- Name: fki_gnd_person_id_fkey; Type: INDEX; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE INDEX fki_gnd_person_id_fkey ON gnd_person_id USING btree (id);
+
+
+--
+-- Name: fki_gnd_place_id_fkey; Type: INDEX; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE INDEX fki_gnd_place_id_fkey ON gnd_place_id USING btree (id);
+
+
+--
+-- Name: fki_gnd_work_id_fkey; Type: INDEX; Schema: mapping; Owner: postgres; Tablespace: 
+--
+
+CREATE INDEX fki_gnd_work_id_fkey ON gnd_work_id USING btree (id);
 
 
 --
@@ -108,11 +230,43 @@ ALTER TABLE ONLY aw_party_id
 
 
 --
--- Name: mapping_aw_person_id_fkey; Type: FK CONSTRAINT; Schema: mapping; Owner: postgres
+-- Name: aw_person_id_fkey; Type: FK CONSTRAINT; Schema: mapping; Owner: postgres
 --
 
 ALTER TABLE ONLY aw_person_id
-    ADD CONSTRAINT mapping_aw_person_id_fkey FOREIGN KEY (id) REFERENCES integrated.person(id);
+    ADD CONSTRAINT aw_person_id_fkey FOREIGN KEY (id) REFERENCES integrated.person(id);
+
+
+--
+-- Name: gnd_occupation_id_fkey; Type: FK CONSTRAINT; Schema: mapping; Owner: postgres
+--
+
+ALTER TABLE ONLY gnd_occupation_id
+    ADD CONSTRAINT gnd_occupation_id_fkey FOREIGN KEY (id) REFERENCES integrated.occupation(id);
+
+
+--
+-- Name: gnd_person_id_fkey; Type: FK CONSTRAINT; Schema: mapping; Owner: postgres
+--
+
+ALTER TABLE ONLY gnd_person_id
+    ADD CONSTRAINT gnd_person_id_fkey FOREIGN KEY (id) REFERENCES integrated.person(id);
+
+
+--
+-- Name: gnd_place_id_fkey; Type: FK CONSTRAINT; Schema: mapping; Owner: postgres
+--
+
+ALTER TABLE ONLY gnd_place_id
+    ADD CONSTRAINT gnd_place_id_fkey FOREIGN KEY (id) REFERENCES integrated.place(id);
+
+
+--
+-- Name: gnd_work_id_fkey; Type: FK CONSTRAINT; Schema: mapping; Owner: postgres
+--
+
+ALTER TABLE ONLY gnd_work_id
+    ADD CONSTRAINT gnd_work_id_fkey FOREIGN KEY (id) REFERENCES integrated.work(id);
 
 
 --
